f95d3f550e663d6a8ba1a76ce20c1cb27498a4e5
calm-mind-priyanka <priyankagogoi1241@gmail.com>
2026-08-19 00:39:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,224 +1,101 @@
-import re
-from os import environ
+import sys
+import glob
+import importlib
+from pathlib import Path
+from pyrogram import idle
+import logging
+import logging.config
+
+# Get logging configurations
+logging.config.fileConfig("logging.conf")
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("imdbpy").setLevel(logging.ERROR)
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("aiohttp").setLevel(logging.ERROR)
+logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
+
+
+from pyrogram import __version__
+from pyrogram.raw.all import layer
+from database.ia_filterdb import Media
+from database.users_chats_db import db
+from info import *
+from utils import temp
 from Script import script
+from datetime import date, datetime
+import pytz
+from aiohttp import web
+from plugins import web_server, check_expired_premium
+import pyrogram.utils
+import asyncio
+from Jisshu.bot import JisshuBot
+from Jisshu.util.keepalive import ping_server
+from Jisshu.bot.clients import initialize_clients
 
-id_pattern = re.compile(r"^.\d+$")
+ppath = "plugins/*.py"
+files = glob.glob(ppath)
+JisshuBot.start()
+loop = asyncio.get_event_loop()
+
+pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
 
-def is_enabled(value, default):
-    if value.lower() in ["true", "yes", "1", "enable", "y"]:
-        return True
-    elif value.lower() in ["false", "no", "0", "disable", "n"]:
-        return False
-    else:
-        return default
-
-
-# Main
-SESSION = environ.get("SESSION", "Media_search")
-API_ID = int(environ.get("API_ID", "24222039"))
-API_HASH = environ.get("API_HASH", "6dd2dc70434b2f577f76a2e993135662")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-PORT = environ.get("PORT", "8082")
-
-# Owners
-ADMINS = [
-    int(admin) if id_pattern.search(admin) else admin
-    for admin in environ.get("ADMINS", "6046055058").split()
-]
-OWNER_USERNAME = environ.get(
-    "OWNER_USERNAME", "Sandymaiwait"
-)  # without @ or https://t.me/
-USERNAME = environ.get("USERNAME", "https://t.me/Sandymaiwait")  # ADMIN USERNAME
-
-# Database Channel
-CHANNELS = [
-    int(ch) if id_pattern.search(ch) else ch
-    for ch in environ.get("CHANNELS", "-1002694840394").split()
-]
-
-# ForceSub Channel & Log Channels
-AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002260068200"))
-AUTH_REQ_CHANNEL = int(environ.get("AUTH_REQ_CHANNEL", "-1002260068200"))
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002433610423"))
-LOG_API_CHANNEL = int(environ.get("LOG_API_CHANNEL", "-1002534471850"))
-LOG_VR_CHANNEL = int(environ.get("LOG_VR_CHANNEL", "-1002395784504"))
-
-# MongoDB
-DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://Sandy4:Sandy4@cluster0.ogfoxmb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get("DATABASE_NAME", "sandy")
-
-# Files index database url
-FILES_DATABASE = environ.get("FILES_DATABASE", "mongodb+srv://lamasandeep821:lmlgZas9Ei9pkZJf@cluster0.u1j1yoa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-COLLECTION_NAME = environ.get("COLLECTION_NAME", "sandy")
-
-# Other Channel's
-SUPPORT_GROUP = int(environ.get("SUPPORT_GROUP", "-1002459015261"))
-DELETE_CHANNELS = int(environ.get("DELETE_CHANNELS", "0"))
-request_channel = environ.get("REQUEST_CHANNEL", "-1002359536802")
-REQUEST_CHANNEL = (
-    int(request_channel)
-    if request_channel and id_pattern.search(request_channel)
-    else None
-)
-MOVIE_UPDATE_CHANNEL = int(environ.get("MOVIE_UPDATE_CHANNEL", "-1002260068200"))
-
-# Added Link Here Not Id
-SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "https://t.me/+4pPWKtxin2YyNTc1")
-MOVIE_GROUP_LINK = environ.get("MOVIE_GROUP_LINK", "https://t.me/+bwi-Oeeg11g2ZmI1")
-
-# Verification
-IS_VERIFY = is_enabled("IS_VERIFY", True)
-# ---------------------------------------------------------------
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/+URI0ggQ1mLAyM2Y1")
-TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/+URI0ggQ1mLAyM2Y1")
-TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/+URI0ggQ1mLAyM2Y1")
-VERIFY_IMG = environ.get(
-    "VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg"
-)
-SHORTENER_API = environ.get("SHORTENER_API", "0eb911f6bd85416d49e665a10688d810bfd5154d")
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "linkshortify.com")
-SHORTENER_API2 = environ.get(
-    "SHORTENER_API2", "204bba345b04f7812f6de7e079f063d9ab3709b9"
-)
-SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "arolinks.com")
-SHORTENER_API3 = environ.get(
-    "SHORTENER_API3", "e2239d738dd541fe55fe74ae6e00c782dc843715"
-)
-SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "CPMShort.com")
-TWO_VERIFY_GAP = int(environ.get("TWO_VERIFY_GAP", "300"))
-THREE_VERIFY_GAP = int(environ.get("THREE_VERIFY_GAP", "300"))
-
-# Language & Quality & Season & Year
-LANGUAGES = [
-    "hindi",
-    "english",
-    "telugu",
-    "tamil",
-    "kannada",
-    "malayalam",
-    "bengali",
-    "marathi",
-    "gujarati",
-    "punjabi",
-    "marathi",
-]
-QUALITIES = [
-    "HdRip",
-    "web-dl",
-    "bluray",
-    "hdr",
-    "fhd",
-    "240p",
-    "360p",
-    "480p",
-    "540p",
-    "720p",
-    "960p",
-    "1080p",
-    "1440p",
-    "2K",
-    "2160p",
-    "4k",
-    "5K",
-    "8K",
-]
-YEARS = [f"{i}" for i in range(2025, 2002, -1)]
-SEASONS = [f"season {i}" for i in range(1, 23)]
-
-# Pictures And Reaction
-START_IMG = (
-    environ.get(
-        "START_IMG",
-        "https://graph.org/file/37024d8bf09ee86288180-2687fda43ba35ba2ec.jpg",
+async def Jisshu_start():
+    print("\n")
+    print("Credit - Telegram @JISSHU_BOTS")
+    bot_info = await JisshuBot.get_me()
+    JisshuBot.username = bot_info.username
+    await initialize_clients()
+    for name in files:
+        with open(name) as a:
+            patt = Path(a.name)
+            plugin_name = patt.stem.replace(".py", "")
+            plugins_dir = Path(f"plugins/{plugin_name}.py")
+            import_path = "plugins.{}".format(plugin_name)
+            spec = importlib.util.spec_from_file_location(import_path, plugins_dir)
+            load = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(load)
+            sys.modules["plugins." + plugin_name] = load
+            print("JisshuBot Imported => " + plugin_name)
+    if ON_HEROKU:
+        asyncio.create_task(ping_server())
+    b_users, b_chats = await db.get_banned()
+    temp.BANNED_USERS = b_users
+    temp.BANNED_CHATS = b_chats
+    await Media.ensure_indexes()
+    me = await JisshuBot.get_me()
+    temp.ME = me.id
+    temp.U_NAME = me.username
+    temp.B_NAME = me.first_name
+    temp.B_LINK = me.mention
+    JisshuBot.username = "@" + me.username
+    JisshuBot.loop.create_task(check_expired_premium(JisshuBot))
+    logging.info(
+        f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}."
     )
-).split()
-FORCESUB_IMG = environ.get("FORCESUB_IMG", "https://graph.org/file/e6c8c517ee3f42d5d772e-331533c6320d361b70.jpg")
-REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split()
-PAYPICS = (
-    environ.get("PAYPICS", "https://graph.org/file/5635f6bd5f76da19ccc70-695af75bfa01aacbf2.jpg")
-).split()
-SUBSCRIPTION = environ.get(
-    "SUBSCRIPTION", "https://graph.org/file/9f3f47c690bbcc67633c2.jpg"
-)
-REACTIONS = ["👀", "😱", "🔥", "😍", "🎉", "🥰", "😇", "⚡"]
+    logging.info(script.LOGO)
+    tz = pytz.timezone("Asia/Kolkata")
+    today = date.today()
+    now = datetime.now(tz)
+    time = now.strftime("%H:%M:%S %p")
+    await JisshuBot.send_message(
+        chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(me.mention, today, time)
+    )
+    await JisshuBot.send_message(
+        chat_id=SUPPORT_GROUP, text=f"<b>{me.mention} ʀᴇsᴛᴀʀᴛᴇᴅ 🤖</b>"
+    )
+    app = web.AppRunner(await web_server())
+    await app.setup()
+    bind_address = "0.0.0.0"
+    await web.TCPSite(app, bind_address, PORT).start()
+    await idle()
 
 
-# Other Funtions
-FILE_AUTO_DEL_TIMER = int(environ.get("FILE_AUTO_DEL_TIMER", "120"))
-AUTO_FILTER = is_enabled("AUTO_FILTER", True)
-IS_PM_SEARCH = is_enabled("IS_PM_SEARCH", True)
-IS_SEND_MOVIE_UPDATE = is_enabled(
-    "IS_SEND_MOVIE_UPDATE", False
-)  # Don't Change It ( If You Want To Turn It On Then Turn It On By Commands) We Suggest You To Make It Turn Off If You Are Indexing Files First Time.
-MAX_BTN = int(environ.get("MAX_BTN", "8"))
-AUTO_DELETE = is_enabled("AUTO_DELETE", True)
-DELETE_TIME = int(environ.get("DELETE_TIME", 120))
-IMDB = is_enabled("IMDB", True)
-FILE_CAPTION = environ.get("FILE_CAPTION", f"{script.FILE_CAPTION}")
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", f"{script.IMDB_TEMPLATE_TXT}")
-LONG_IMDB_DESCRIPTION = is_enabled("LONG_IMDB_DESCRIPTION", False)
-PROTECT_CONTENT = is_enabled("PROTECT_CONTENT", False)
-SPELL_CHECK = is_enabled("SPELL_CHECK", True)
-LINK_MODE = is_enabled("LINK_MODE", True)
-TMDB_API_KEY = environ.get("TMDB_API_KEY", "")
-
-# Online Streaming And Download
-STREAM_MODE = is_enabled("STREAM_MODE", True)
-
-MULTI_CLIENT = False
-SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))
-PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-if "DYNO" in environ:
-    ON_HEROKU = True
-else:
-    ON_HEROKU = False
-URL = environ.get("FQDN", "")
-
-# Commands
-admin_cmds = [
-    "/add_premium - Add A User To Premium",
-    "/premium_users - View All Premium Users",
-    "/remove_premium - Remove A User's Premium Status",
-    "/add_redeem - Generate A Redeem Code",
-    "/refresh - Refresh Free Trail",
-    "/set_muc - Set Movie Update Channel",
-    "/pm_search_on - Enable PM Search",
-    "/pm_search_off - Disable PM Search",
-    "/set_ads - Set Advertisements",
-    "/del_ads - Delete Advertisements",
-    "/setlist - Set Top Trending List",
-    "/clearlist - Clear Top Trending List",
-    "/verify_id - Verification Off ID",
-    "/index - Index Files",
-    "/send - Send Message To A User",
-    "/leave - Leave A Group Or Channel",
-    "/ban - Ban A User",
-    "/unban - Unban A User",
-    "/broadcast - Broadcast Message",
-    "/grp_broadcast - Broadcast Messages To Groups",
-    "/delreq - Delete Join Request",
-    "/channel - List Of Database Channels",
-    "/del_file - Delete A Specific File",
-    "/delete - Delete A File(By Reply)",
-    "/deletefiles - Delete Multiple Files",
-    "/deleteall - Delete All Files",
-]
-
-cmds = [
-    {"start": "Start The Bot"},
-    {"most": "Get Most Searches Button List"},
-    {"trend": "Get Top Trending Button List"},
-    {"mostlist": "Show Most Searches List"},
-    {"trendlist": "𝖦𝖾𝗍 𝖳𝗈𝗉 𝖳𝗋𝖾𝗇𝖽𝗂𝗇𝗀 𝖡𝗎𝗍𝗍𝗈𝗇 𝖫𝗂𝗌t"},
-    {"plan": "Check Available Premium Membership Plans"},
-    {"myplan": "Check Your Currunt Plan"},
-    {"refer": "To Refer Your Friend And Get Premium"},
-    {"stats": "Check My Database"},
-    {"id": "Get Telegram Id"},
-    {"font": "To Generate Cool Fonts"},
-    {"details": "Check Group Details"},
-    {"settings": "Change Bot Setting"},
-    {"grp_cmds": "Check Group Commands"},
-    {"admin_cmds": "Bot Admin Commands"},
-]
+if __name__ == "__main__":
+    try:
+        loop.run_until_complete(Jisshu_start())
+    except KeyboardInterrupt:
+        logging.info("Service Stopped Bye 👋")
