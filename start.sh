fbfd16d915af3b25514aae0a1817041b9bd9ea6b
calm-mind-priyanka <priyankagogoi1241@gmail.com>
2026-08-19 00:23:46 +0530

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,12 +1,6 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/JisshuTG/Jisshu-filter-bot /Jisshu-filter-bot
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Jisshu-filter-bot
-fi
-cd /Jisshu-filter-bot
+#!/bin/sh
+set -e
+cd "$(dirname "$0")"
 pip3 install -U -r requirements.txt
 echo "Starting Jisshu filter bot...."
-python3 bot.py
+exec python3 bot.py
