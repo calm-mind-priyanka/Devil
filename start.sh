61ba00b89ac0bdb5ffdc93343ea8892e871a16be
calm-mind-priyanka <priyankagogoi1241@gmail.com>
2026-08-19 00:41:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
-set -e
-cd "$(dirname "$0")"
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/JisshuTG/Jisshu-filter-bot /Jisshu-filter-bot
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Jisshu-filter-bot
+fi
+cd /Jisshu-filter-bot
 pip3 install -U -r requirements.txt
 echo "Starting Jisshu filter bot...."
-exec python3 bot.py
+python3 bot.py
